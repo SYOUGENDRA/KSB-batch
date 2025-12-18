bc654aca8d04251a565b753bc6cf711d0d144f8e
Yougendra <Yougendra@LAPTOP-VUSN12VU>
2025-12-18 08:16:53 +0530

    First time commited

---
+++ src/test/java/Demo/PracTest.java
@@ -0,0 +1,15 @@
+package Demo;
+
+import org.testng.annotations.Test;
+
+public class PracTest {
+
+	
+	@Test(groups="Regression")
+	
+	public void BikeTest() {
+		
+		
+		System.out.println("KTM");
+	}
+}
